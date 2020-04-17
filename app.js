3a0a8364423d7ddfbfc85731017924d113a93fe4
Umair-Rana <umairrana109.1@gmail.com>
2020-04-18 00:40:24 +0500

    Validation and Session Unsuccessfull deployment

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
+var expressValidator = require('express-validator')
+var expressSession = require('express-session')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,16 +14,17 @@ var detailRouter = require('./routes/detail');
 
 var app = express();
 
-// view engine setup
+//view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/users/detail', detailRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Validation Form', success: false, errors: req.session.errors });
+  req.session.errors= null;
 });
 
-router.get('/test/:id', function(req, res, next) {
-  res.render('test', {output: req.params.id} )
-})
+router.post('/submit', function(req, res, next) {
+  //Check Validity.
+  req.check('email', 'invalid email address').isEmail();
+  req.check('password', 'invalid password').isLength({min:4}).equals(req.body.confirmPassword);
 
-router.post('/test/submit', function(req, res, next) {
-  var id = req.body.id;
-  res.redirect('/test/'+ id)
-})
+  var errors = req.validationErrors();
+  if(errors){
+    req.session.errors = errors;
+    req.session.success = false;
+  }else{
+    req.session.success = true;
+  }
+  res.redirect('/');
+});
 
 module.exports = router;
